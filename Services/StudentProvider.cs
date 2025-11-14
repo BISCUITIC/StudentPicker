24c00c7df1eb3a9b52a87154ab62c2af62f8a8ff
BISCUITIC <tort24457@gamil.com>
2025-11-14 19:07:20 +0300

    add services that receives students

--- Services/StudentProvider.cs
+++ Services/StudentProvider.cs
@@ -1,7 +1,29 @@
-﻿namespace Services;
+﻿using Data;
+using Data.Model;
+using Microsoft.EntityFrameworkCore;
 
-internal class StudentProvider
+namespace Services;
+
+internal class IncorrectGroupData : Exception
 {
+    public IncorrectGroupData(string? message) : base(message) { }
+}
 
-    public StudentProvider() { }
+internal class StudentProvider : IStudentProvider
+{
+    private readonly ApplicationContext _context;
+
+    public StudentProvider(ApplicationContext applicationContext) 
+    {
+        _context = applicationContext;
+    }
+
+    public List<Student> GetStudents(int groupNumber, char groupLetter)
+    {
+        return _context.Students  
+                       .Include(student => student.StudyGroup)
+                       .Where(student => student.StudyGroup.Number == groupNumber &&
+                                         student.StudyGroup.Letter == groupLetter)
+                       .ToList();
+    }
 }
