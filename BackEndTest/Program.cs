5548a69bd24ebdb3ca7bf321e195b22d17b6d230
BISCUITIC <tort24457@gamil.com>
2025-11-14 19:38:57 +0300

    refactor

--- BackEndTest/Program.cs
+++ BackEndTest/Program.cs
@@ -1,44 +1,22 @@
 ﻿using Data;
 using Data.ConnectionConfig;
-using Data.Model;
-using Microsoft.EntityFrameworkCore;
-
-using Services;
+using Services.Providers.Srudent;
 
 namespace BackEndTest;
 
 internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        //Group group1 = new Group(11, 'A');
-        //Group group2 = new Group(11, 'Б');
-        //Group group3 = new Group(11, 'В');
-        //Group group4 = new Group(11, 'A');
-
-
-
-
-        //using (ApplicationContext context = new ApplicationContext())
-        //{
-        //    var group = context.Groups.FirstOrDefault(group => group.Id == 1);
-        //    Student student1 = new Student("Тимлфей", "Сосновский", group);
-        //    Student student2 = new Student("Артемий", "Шелюто", group);
-        //    Student student3 = new Student("Тима", "Потапенко", group);
-
-        //    context.AddRange(student1, student2, student3);
-        //    context.SaveChanges();
-        //}
         ConnectionStringProvider connection = new ConnectionStringProvider();
         ApplicationContext context = new ApplicationContext(connection);
 
         StudentProvider studentProvider = new StudentProvider(context);
-        
+
         foreach (var s in studentProvider.GetStudents(11, 'A'))
         {
             Console.WriteLine(s.FullName);
         }
-
 
         context.Dispose();
     }
