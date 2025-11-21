927726310291a0f12628af9ca3a906a0b7379cd2
BISCUITIC <tort24457@gamil.com>
2025-11-21 18:39:28 +0300

    refactor

--- BackEndTest/Program.cs
+++ BackEndTest/Program.cs
@@ -1,39 +1,39 @@
-﻿    using Application.Interfaces;
-    using Domain.Entities;
-    using Infrastructure;
-    using Infrastructure.Repositories;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-    namespace BackEndTest;
+namespace BackEndTest;
 
-    internal class Program
+internal class Program
+{
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        IConfigurationRoot connection = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json")
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .Build();
+
+        ServiceCollection services = new ServiceCollection();
+        services.AddSingleton<IConfigurationRoot>(connection);
+        services.AddDbContext<ApplicationContext>();
+
+        services.AddScoped<IGroupRepository, GroupRepository>();
+        services.AddScoped<IStudentRepository, StudentRepository>();
+
+        var provider = services.BuildServiceProvider();
+
+        var groupProvider = provider.GetRequiredService<IGroupRepository>();
+        var studentProvider = provider.GetRequiredService<IStudentRepository>();
+
+        Group group = groupProvider.GetById(1) ?? throw new Exception();
+        List<Student> students = studentProvider.GetAllByGroup(group);
+
+        foreach (Student student in students)
         {
-            IConfigurationRoot connection = new ConfigurationBuilder()
-                                            .AddJsonFile("appsettings.json")
-                                            .SetBasePath(Directory.GetCurrentDirectory())
-                                            .Build();
-
-            ServiceCollection services = new ServiceCollection();
-            services.AddSingleton<IConfigurationRoot>(connection);        
-            services.AddDbContext<ApplicationContext>();
-
-            services.AddScoped<IGroupRepository, GroupRepository>();
-            services.AddScoped<IStudentRepository, StudentRepository>();
-
-            var provider = services.BuildServiceProvider();
-
-            var groupProvider = provider.GetRequiredService<IGroupRepository>();  
-            var studentProvider = provider.GetRequiredService<IStudentRepository>();
-
-            Group group = groupProvider.GetById(1) ?? throw new Exception();
-            List<Student> students = studentProvider.GetAllByGroup(group);
-
-            foreach (Student student in students)
-            {
-                Console.WriteLine(student.FullName);
-            }        
+            Console.WriteLine(student.FullName);
         }
     }
+}
