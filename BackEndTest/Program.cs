d98997114586d4f2688582b91f6a20c8d8ae2c1b
BISCUITIC <tort24457@gamil.com>
2025-11-21 18:27:21 +0300

    add dependency injection

--- BackEndTest/Program.cs
+++ BackEndTest/Program.cs
@@ -1,24 +1,32 @@
-﻿using Application.Interfaces;
-using Domain.Entities;
-using Infrastructure;
-using Infrastructure.Repositories;
-using Microsoft.Extensions.Configuration;
+﻿    using Application.Interfaces;
+    using Domain.Entities;
+    using Infrastructure;
+    using Infrastructure.Repositories;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
 
-namespace BackEndTest;
+    namespace BackEndTest;
 
-internal class Program
-{
-    private static void Main(string[] args)
+    internal class Program
     {
-        IConfigurationRoot connection = new ConfigurationBuilder()
-                          .AddJsonFile("appsettings.json")
-                          .SetBasePath(Directory.GetCurrentDirectory())
-                          .Build(); ;
-
-        using (ApplicationContext context = new ApplicationContext(connection))
+        private static void Main(string[] args)
         {
-            IGroupRepository groupProvider = new GroupRepository(context);
-            IStudentRepository studentProvider = new StudentRepository(context);
+            IConfigurationRoot connection = new ConfigurationBuilder()
+                                            .AddJsonFile("appsettings.json")
+                                            .SetBasePath(Directory.GetCurrentDirectory())
+                                            .Build();
+
+            ServiceCollection services = new ServiceCollection();
+            services.AddSingleton(connection);        
+            services.AddDbContext<ApplicationContext>();
+
+            services.AddScoped<IGroupRepository, GroupRepository>();
+            services.AddScoped<IStudentRepository, StudentRepository>();
+
+            var provider = services.BuildServiceProvider();
+
+            var groupProvider = provider.GetRequiredService<IGroupRepository>();  
+            var studentProvider = provider.GetRequiredService<IStudentRepository>();
 
             Group group = groupProvider.GetById(1) ?? throw new Exception();
             List<Student> students = studentProvider.GetAllByGroup(group);
@@ -26,7 +34,6 @@ internal class Program
             foreach (Student student in students)
             {
                 Console.WriteLine(student.FullName);
-            }
+            }        
         }
     }
-}
