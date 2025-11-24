e387bd895af6acdb4fb257c64de54b617c41b633
BISCUITIC <tort24457@gamil.com>
2025-11-24 20:52:44 +0300

    configured DI in presentation

--- Presentation/App.xaml.cs
+++ Presentation/App.xaml.cs
@@ -1,15 +1,49 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Application.Interfaces;
+using Infrastructure;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
 using System.Windows;
 
-namespace Presentation
+namespace Presentation;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : System.Windows.Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private readonly IHost _host;
+
+    public App()
     {
-        //private readonly 
+        IConfigurationRoot connection = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json")
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .Build();
+        _host = Host.CreateDefaultBuilder()
+                    .ConfigureServices((context, services) =>
+                    {
+                        services.AddSingleton<IConfigurationRoot>(connection);
+                        services.AddDbContext<ApplicationContext>();
+
+                        services.AddScoped<IGroupRepository, GroupRepository>();
+                        services.AddScoped<IStudentRepository, StudentRepository>();
+                        
+                        services.AddSingleton<MainWindow>();
+                    }).Build();
     }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {        
+        MainWindow window = _host.Services.GetRequiredService<MainWindow>();
+        window.Show();  
+        base.OnStartup(e);
+    }
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _host.Dispose();        
+        base.OnExit(e);
+    }
 }
