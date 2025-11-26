8330319349233714fe2be34c14b15dfdb597667c
BISCUITIC <tort24457@gamil.com>
2025-11-26 20:25:25 +0300

    add student provider

---
+++ Applications/Services/StudentProvider.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services;
+
+internal class StudentProvider
+{
+    private readonly IStudentRepository _studentRepository;
+
+    public StudentProvider(IStudentRepository studentRepository)
+    {
+        _studentRepository = studentRepository;
+    }
+
+    public IReadOnlyCollection<Student> GetStudents(Group group)
+    {
+        return _studentRepository.GetAllByGroup(group);
+    }
+}
