67a400e6adc445e3bc6944a81371e3a6f7949a64
BISCUITIC <tort24457@gamil.com>
2025-12-02 22:51:40 +0300

    Rename GroupService and StudentServices

---
+++ Applications/Services/GroupService.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services;
+
+public class GroupService
+{
+    private readonly IGroupRepository _groupRepository;
+
+    public GroupService(IGroupRepository groupRepository)
+    {
+        _groupRepository = groupRepository;
+    }
+
+    public IReadOnlyCollection<Group> GetAllGroups()
+    {
+        return _groupRepository.GetAll();
+    }
+}
---
+++ Applications/Services/StudentService.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services;
+
+public class StudentService
+{
+    private readonly IStudentRepository _studentRepository;
+
+    public StudentService(IStudentRepository studentRepository)
+    {
+        _studentRepository = studentRepository;
+    }
+
+    public IReadOnlyCollection<Student> GetStudents(int groupId)
+    {
+        return _studentRepository.GetAllByGroupId(groupId);
+    }
+    public Student GetStudent(int studentId)
+    {
+        return _studentRepository.GetById(studentId);
+    }
+    public void UpdateStudent(Student student)
+    {
+        _studentRepository.Update(student);
+    }
+}
