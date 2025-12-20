2ce7f1f26b0a24db0e835c2a69c5d8d8e588a467
BISCUITIC <tort24457@gamil.com>
2025-12-20 18:25:06 +0300

    refactor xaml code, add context menu

---
+++ Presentation/AttachedProperties/ButtonProperties.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Presentation.AttachedProperties;
+
+public static class ButtonProperties
+{
+    public static readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.RegisterAttached(
+            "CornerRadius",
+            typeof(CornerRadius),
+            typeof(ButtonProperties),
+            new FrameworkPropertyMetadata(new CornerRadius(0),
+                                              FrameworkPropertyMetadataOptions.Inherits)
+        );
+
+    public static void SetCornerRadius(DependencyObject element, CornerRadius value)
+        => element.SetValue(CornerRadiusProperty, value);
+
+    public static CornerRadius GetCornerRadius(DependencyObject element)
+        => (CornerRadius)element.GetValue(CornerRadiusProperty);
+}
