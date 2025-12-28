613765df2bd31af8771d08a6258e57cd452e8d87
BISCUITIC <tort24457@gamil.com>
2025-12-28 19:25:51 +0300

    fix and optimize random generator

--- Applications/Services/RandomChoiceProvider.cs
+++ Applications/Services/RandomChoiceProvider.cs
@@ -4,22 +4,13 @@ namespace Application.Services;
 
 public class RandomChoiceProvider : IRandomProvider
 {
-    private readonly Random _random;
-
-    public RandomChoiceProvider()
-    {
-        _random = new Random();
-    }
-
     public int? Next(IReadOnlyCollection<int> selection)
-    {
-        List<int> available  = selection.ToList();        
-
-        if(available.Count == 0)
+    {        
+        if (selection.Count == 0)
             return null;
-
-        int randomIndex = _random.Next(0, available.Count);
-
-        return available[randomIndex];
+        
+        int randomIndex = Random.Shared.Next(selection.Count);
+        
+        return selection.ElementAt(randomIndex);
     }
 }
